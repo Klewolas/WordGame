da7e86e4022c1b3f4c5ccba507bab2f1a5d4dcb0
Klewolas <berke.altintas@hotmail.com>
2023-01-05 21:27:08 +0300

    When update score play dotween.

--- Assets/Scripts/ScoreUpdateListener.cs
+++ Assets/Scripts/ScoreUpdateListener.cs
@@ -1,13 +1,16 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class ScoreUpdateListener : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
+    private Sequence _sequence;
 
     void Start()
     {
         ScoreManager.ScoreIncreased += ScoreTextUpdate;
+        _sequence = DOTween.Sequence();
     }
 
     private void OnDestroy()
@@ -17,6 +20,8 @@ public class ScoreUpdateListener : MonoBehaviour
 
     void ScoreTextUpdate(int score)
     {
+        _sequence.Append(transform.DOScale(new Vector3(3f, 3f, 1), 0.3f)).Append(transform.DOScale(new Vector3(1,1,1), 0.3f));
+        _sequence.Play();
         _scoreText.text = "Score : " + score;
     }
 }
