44a5271e24ed1b7e8426992035cd3e07324e0981
Klewolas <berke.altintas@hotmail.com>
2023-01-19 23:19:28 +0300

    LevelNumber added to leveldata scriptableobjects.

--- Assets/Scripts/ModelScripts/LevelData.cs
+++ Assets/Scripts/ModelScripts/LevelData.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Level", menuName = "ScriptableObjects/Level", order = 1)]
 public class LevelData : ScriptableObject
 {
+    [SerializeField] public int LevelNumber;
+    
     [SerializeField] public List<string> Words;
 
     [SerializeField] public float SpawnTime;
 
     [SerializeField] public int LevelWinScore;
 
-    [SerializeField] public int LevelTime;
+    [SerializeField] public int Life = 5;
 
     [SerializeField] public float WordVelocity;
 }
--- Assets/Scripts/ModelScripts/PlayerData.cs
+++ Assets/Scripts/ModelScripts/PlayerData.cs
@@ -2,4 +2,9 @@
 public class PlayerData
 {
     public int CurrentLevel;
+
+    public PlayerData(int currentLevel)
+    {
+        CurrentLevel = currentLevel;
+    }
 }
